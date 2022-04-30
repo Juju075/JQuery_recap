00007ffb3c7e88f4b86299923c47e0974cdadadd
elgato <amzfba.1bestbuy@gmail.com>
2022-04-30 12:23:54 +0200

    jquery not working

--- Js/JQ.js
+++ Js/JQ.js
@@ -1,3 +1,15 @@
+/** JQuery */
 function func1() {
   $("#img1").fadeToogle();
+}
+
+
+/** Javascript  */
+function test1() {
+  alert("Hello! I am an alert box!!");
+}
+
+/** Javascript ok */
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
 }
